5b890928c2674b2d0be4ff9a8ad25b682fd527ba
sergeantkawaii <waleedhofioni@hotmail.com>
2023-11-02 00:30:29 -0400

    blackjack buttno

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -9,32 +9,14 @@ function clearLog(){
 
 }
 var hit2 = 0;
-function playGame() {
-    var inputField = document.getElementById('input-field');
-    var submitButton = document.getElementById('submit');
-
-    
-    inputField.addEventListener('keypress', function (x){
-
-        if (x.key === "Enter")
-        {
-            hitFunc();
-            
-        }
-    })
-    submitButton.addEventListener('click', function() {
-        hitFunc();
-    })
+    var hit = document.getElementById('hit')
+    var stand = document.getElementById('stand')
 
 
-   
-
-     function hitFunc(){
-        var card = inputField.value; // Get the user's input value
-    
-        
-        if (card === "hit") {
+        hit.addEventListener("click", function playGame(){
             let randomNumber = Math.floor(Math.random() * 10) + 1; // Generate a random card value (1-10)
+
+            
             hit2 += randomNumber;
             updateLog("You drew a " + randomNumber + ". Your hand: " + hit2);
 
@@ -45,20 +27,16 @@ function playGame() {
                 updateLog("Nice, you got "+hit2);
                 test();
             }
-        } else if (card === "stand") {
+        });  
+    
+        stand.addEventListener("click", function(){
             updateLog("You have " + hit2 + " hand");
             test();
-        } 
-        else 
-        {
-            updateLog("incorrect entry")
-            
-        }
+        })
+        
+        
     
-        // Clear the input field after each action
-        inputField.value = "";
-    }
-}
+
 
 
 function test() {
@@ -84,31 +62,22 @@ function test() {
     } else {
         updateLog("looks like it's a draw")
     }
+    
 }
 
 function endGame() {
     // Disable the submit button when the game ends
-    document.getElementById('submit').disabled = true;
-    hit2 = 0;           
+    //document.getElementById('submit').disabled = true;
+    hit2 = 0;  
+         
+        
 }
 
 document.getElementById('start-game').addEventListener('click', function () {
     // Enable the submit button and start the game
     hit2 = 0;
-    document.getElementById('submit').disabled = false;
+    //document.getElementById('submit').disabled = false;
     clearLog();
     updateLog("Game started. You can hit or stand.");
     playGame();
 });
-
-
-
-
-
-
-
-const testing = document.getElementById('backButton').addEventListener('click', function(){
-
-    updateLog("BYE =D")
-    window.location.href ="https://www.google.com"
-    })
